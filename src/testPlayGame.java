0a5641bc3ce004b3350ac87e2a6d0e90d31518aa
Nguyen-Van0405 <nguyenthvan45@gmail.com>
2022-11-13 21:33:03 +0700

    test play game button

--- src/testPlayGame.java
+++ src/testPlayGame.java
@@ -1,14 +1,14 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SeleniumTesting {
+public class testPlayGame {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","src/ChromeDriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
-        driver.get("http://127.0.0.1:5000/");
+        driver.get("http://127.0.0.1:5000/gamePage/3");
 
+        driver.findElement(By.xpath("//a[text()=' Play']")).click();
     }
-
-
 }
